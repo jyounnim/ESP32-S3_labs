52ac41e405f87d47d2900bfd3300c3660e3b14e4
Jaeyoun You <jyounnim@gmail.com>
2026-09-14 11:01:07 +0900

    Remove old_draft in '02_WAKE_WORD_LAB'

--- 05_AI/02_WAKE_WORD_LAB/_old_draft/01_Step_1._microspeech_예제로_파이프라인_검증.sh
+++
@@ -1,4 +0,0 @@
-# Source: 02_WAKE_WORD_LAB.md
-# Section: Step 1. micro_speech 예제로 파이프라인 검증
-
-cp -r managed_components/espressif__esp-tflite-micro/examples/micro_speech/main/* main/
--- 05_AI/02_WAKE_WORD_LAB/_old_draft/02_Step_1._microspeech_예제로_파이프라인_검증.sh
+++
@@ -1,5 +0,0 @@
-# Source: 02_WAKE_WORD_LAB.md
-# Section: Step 1. micro_speech 예제로 파이프라인 검증
-
-idf.py build
-idf.py -p <포트> flash monitor
